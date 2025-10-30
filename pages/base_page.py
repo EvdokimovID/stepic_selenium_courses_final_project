a39dda0e1c1dabf8bad763c76335ce1373999043
EvdokimovID <vancouver.evdokimov@yandex.ru>
2025-10-30 19:06:22 +0500

    Reorganized page-objects system and added tests for existing and go to login page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,12 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import (
-    NoSuchElementException,  
-    NoAlertPresentException,
+    NoSuchElementException,
     TimeoutException
 )
 import time
 import math
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -47,3 +47,10 @@ class BasePage():
         answer = str(math.log(abs((12 * math.sin(float(x))))))
         alert.send_keys(answer)
         alert.accept()
+    
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+        
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
